4cadee648ea7b73fcb454fc7a1091492f3ead6e3
heshanpasindusankalpa <pasindusankalpa2021@gmail.com>
2025-08-05 17:45:40 +0530

    Create NavBar.js

---
+++ src/components/Navigation/NavBar.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import './navBar.css';
+import Logo from '../../Images/logo.png';
+import { Link } from 'react-router-dom';
+
+export default function NavBar() {
+  return (
+    <div className='navBarBox'>
+      <div className='navBarLeft'>
+        
+        <img src={Logo} alt="Logo" className='navBarLogo' />
+        <span className='name1'>Face</span>
+        <span className='name2'>Entry</span>
+      </div>
+      <div className='navBarCenter'>
+         <div className='navBarLinks'>
+         
+          <Link to="/" className='navBarLink'>Home</Link>
+          <Link to="/about" className='navBarLink'>About</Link>
+         </div>
+      </div>
+      <div className='navBarRight'>
+        <button className='RegisterButton'>Register</button>
+       
+       
+        
+      </div>
+    </div>
+  );
+}
