1ce631223335445e8cba6bb947502ce59d0e008b
heshanpasindusankalpa <pasindusankalpa2021@gmail.com>
2025-08-18 17:13:29 +0530

    Update router.js

--- expressBackend/router.js
+++ expressBackend/router.js
@@ -6,6 +6,10 @@ const controller = require('./controller');
 router.post('/register-admin', controller.registerAdmin);
 router.post('/register-employee', controller.registerEmployee);
 router.post('/mark-attendance', controller.markAttendance);
+router.get('/employees', controller.listEmployees);              // NEW (list with optional query)
+router.put('/employees/:employeeId', controller.updateEmployee); // NEW
+router.delete('/employees/:employeeId', controller.deleteEmployee); // NEW
+
 router.post('/login', controller.login);
 router.get('/get-employees', controller.getEmployees);
 router.get('/get-employees-with-encodings', controller.getEmployeesWithEncodings);
