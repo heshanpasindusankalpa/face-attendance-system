feea668a83e2579ddb152c5abda0572ecbf80f8a
heshanpasindusankalpa <pasindusankalpa2021@gmail.com>
2025-08-19 22:02:09 +0530

    Update server.js

--- expressBackend/server.js
+++ expressBackend/server.js
@@ -19,7 +19,7 @@ app.use(helmet());
 
 app.use(cors({
   origin: 'http://localhost:3000',
-  methods: ['GET', 'POST', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH','OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }));
